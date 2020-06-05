ef683448135647f9dd76cebd1945053599541b5a
Arka Patra <arkapatra31@gmail.com>
2020-06-05 22:37:19 +0530

    Update Inversions.py

--- Inversions.py
+++ Inversions.py
@@ -27,22 +27,7 @@ def count_inverions(A):
     S, A = count_split_inversions(A1, A2)
     return L+S+R, A
 
-f = open('./inv.txt', 'r')
-Arr = []
-strng = f.read()
-f.close()
-strt, end = 0, 0
-i = 0
-while(i<len(strng)):
-    if(strng[i] == '\n'):
-        end = i
-        no = int(strng[strt:end])
-        print(no)
-        Arr = Arr + [no]
-        strt = i+1
-    i = i+1
 
-print('Length:', len(Arr))
+Arr = [3,8,9,2,1,6,5,4,0,10]
 inv, Srt = count_inverions(Arr)
 print('No. of inversions : ',inv)
-print(Srt[99950:])
